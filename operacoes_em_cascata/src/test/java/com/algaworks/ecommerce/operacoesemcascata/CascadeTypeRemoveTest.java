054ccc3455b4e1d96a0a05252a93453c0d890cec
hvivox <hvivox@hotmail.com>
2023-05-17 09:31:24 -0400

    8.8. Removendo objetos órfãos com a propriedade orphanRemoval

--- operacoes_em_cascata/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
+++ operacoes_em_cascata/src/test/java/com/algaworks/ecommerce/operacoesemcascata/CascadeTypeRemoveTest.java
@@ -1,14 +1,27 @@
 package com.algaworks.ecommerce.operacoesemcascata;
 
 import com.algaworks.ecommerce.EntityManagerTest;
-import com.algaworks.ecommerce.model.ItemPedido;
-import com.algaworks.ecommerce.model.ItemPedidoId;
-import com.algaworks.ecommerce.model.Pedido;
-import com.algaworks.ecommerce.model.Produto;
+import com.algaworks.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CascadeTypeRemoveTest extends EntityManagerTest {
+
+    // @Test
+    public void removerItensOrfaos() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+        Assert.assertFalse(pedido.getItens().isEmpty());
+
+        entityManager.getTransaction().begin();
+        pedido.getItens().clear();
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertTrue(pedidoVerificacao.getItens().isEmpty());
+    }
 
     @Test
     public void removerRelacaoProdutoCategoria() {
