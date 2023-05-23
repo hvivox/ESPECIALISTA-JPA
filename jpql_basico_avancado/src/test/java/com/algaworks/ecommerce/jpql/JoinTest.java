64c940d240f01abb38b63458b971d20a745c22bc
hvivox <hvivox@hotmail.com>
2023-05-23 10:03:54 -0400

    9.8. Fazendo o join e usando o fetch

--- jpql_basico_avancado/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
+++ jpql_basico_avancado/src/test/java/com/algaworks/ecommerce/jpql/JoinTest.java
@@ -11,6 +11,21 @@ import java.util.List;
 public class JoinTest extends EntityManagerTest {
     
     @Test
+    public void usarJoinFetch() {
+        String jpql = "select p from Pedido p "
+                + " left join fetch p.pagamento "
+                + " join fetch p.cliente "
+                + " left join fetch p.notaFiscal "
+                ;
+        
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+    
+    
+    @Test
     public void fazerLeftJoin() {
         String jpql = "select p from Pedido p left join p.pagamento pag on pag.status = 'PROCESSANDO'";
         
