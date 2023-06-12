439ab3d8c6d7fc4d7f93bf14c7f5d487e71b9f3f
hvivox <hvivox@hotmail.com>
2023-06-12 09:34:26 -0400

    10.3-10.4 | utilizando projeções para selecionar colunas especificas da consulta

--- jpql_basico_avancado/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
+++ jpql_basico_avancado/src/test/java/com/algaworks/ecommerce/criteria/BasicoCriteriaTest.java
@@ -11,7 +11,52 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
-
+    
+    
+    
+    @Test
+    public void projetarOResultado() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+        
+        criteriaQuery.multiselect(root.get("id"), root.get("nome"));
+        
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+        
+        lista.forEach(arr -> System.out.println("ID: " + arr[0] + ", Nome: " + arr[1]));
+    }
+    
+    @Test
+    public void retornarTodosOsProdutosExercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+        
+        criteriaQuery.select(root);
+        
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+    
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        
+        criteriaQuery.select(root.get("total"));
+        
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+        
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal total = typedQuery.getSingleResult();
+        Assert.assertEquals(new BigDecimal("2398.00"), total);
+    }
+    
     @Test
     public void buscarPorIdentificador() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
